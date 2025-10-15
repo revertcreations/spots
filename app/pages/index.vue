8616b784529c4be822471c4bba4c9a36e7ee40a5
revertcreations <revertcreations@gmail.com>
2025-10-15 11:39:05 -0400

    add landing page, navbar, and other layouts

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto my-8">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Spots!
+        </h1>
+        <p class="py-6">
+          Keep track of spots you find in the wild, what tricks have been done there, and
+          tricks you want to try. Make lists of trick selections for your next session, ensuring
+          your next part is dialed in!
+        </p>
+        <button class="btn btn-accent">
+          Sign In
+          <Icon name="tabler:brand-github" class="inline-block ml-2" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
